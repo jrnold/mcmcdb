6d697e6aca55da6a4a8da2933192f44fa8827d4d
Jeff Arnold <jeffrey.arnold@gmail.com>
2012-10-02 10:20:17 -0400

    updated line data

--- R/csv.R
+++ R/csv.R
@@ -1,5 +1,5 @@
 mcmc_from_csv <- function(file, start=1, end=NULL, thin=1,  ...) {
-    x <- as.matrix(read.csv(file))
+    x <- as.matrix(read.csv(file, ...))
     if (is.null(end)) {
         end <- nrow(x)
     }
@@ -9,5 +9,5 @@ mcmc_from_csv <- function(file, start=1, end=NULL, thin=1,  ...) {
 mcmc_list_from_csv <- function(file, start=1, end=NULL, thin=1,  ...) {
     new("mcmc.list",
         lapply(file, mcmc_from_csv,
-               start=start, end=end, thin=thin, ... = ...))
+               start=start, end=end, thin=thin, ...))
 }
--- inst/line/run_line.sh
+++ inst/line/run_line.sh
@@ -3,5 +3,5 @@ make
 for i in {1..2}
 do
     echo $i
-    ./line.stanx --data=data.R --iter=200 --samples=chain_$i.csv
+    ./line.stanx --data=data.R --seed=1234567  --warmup=2000 --iter=2200 --samples=chain_$i.csv
 done
