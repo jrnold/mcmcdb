f82f1cb24175336efce4b9a1416035be0245526b
Jeff Arnold <jeffrey.arnold@gmail.com>
2012-10-12 01:24:35 -0400

    added docs

--- R/methods-mcmcByParameter.R
+++ R/methods-mcmcByParameter.R
@@ -1,21 +1,17 @@
-
-# ---------------------------------
-
-setGeneric("mcmcDoParameter",
-           function(object, parameter, ...) {
-               standardGeneric("mcmcDoParameter")
-           })
-
-mcmc_do_parameter <- function(object, parameter, .fun=identity, ...) {
-    .fun(x[x$parameter == parameter, ]$value)
-}
-
-setMethod("mcmcDoParameter",
-          signature=c(object="McmcLong", parameter="character"),
-          mcmc_do_parameter)
-
-# --------------------------------
-
+##' Apply function to the MCMC samples of each parameter
+##'
+##' @param object MCMC object
+##' @param .fun Function to apply to the vector of parameter values
+##' from all iterations.
+##' @return \code{list} of length equal to the number of parameter.
+##' Each element contains the results of \code{.fun} for that
+##' parameter.
+##'
+##' @rdname mcmcByParameter-method
+##' @name mcmcByParameter-method
+##' @docType methods
+##' @keywords methods
+##' @aliases mcmcByParameter,McmcLong-method
 setGeneric("mcmcByParameter",
            function(object, ...) {
                standardGeneric("mcmcByParameter")
@@ -23,7 +19,7 @@ setGeneric("mcmcByParameter",
 
 mcmc_by_parameter <- function(object, .fun=identity, ...) {
     f <- function(object) .fun(x$value)
-    ddply(object, "parameter", .fun=f, ...)
+    dlply(object@samples, "parameter", .fun=f, ...)
 }
 
 setMethod("mcmcByParameter", "McmcLong", mcmc_by_parameter)
