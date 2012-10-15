52bcb593f39d6cd684791fc4892ef16cec0e438b
Jeff Arnold <jeffrey.arnold@gmail.com>
2012-10-15 15:23:42 -0400

    update docs, typo fix

--- R/00-package.R
+++ R/00-package.R
@@ -1,15 +1,15 @@
-g#' Some S4 classes for MCMC sample data objects
-#'
-#' S4 wrappers of the classes to store mcmc objects in \pkg{coda},
-#' as well as a few new classes.
-#'
-#' @name mcmc4
-#' @docType package
-#' @import plyr
-#' @import reshape2
-#' @import stringr
-#' @import DataFramePlus
-#' @importMethodsFrom stats4 coef vcov
+##' Some S4 classes for MCMC sample data objects
+##'
+##' S4 wrappers of the classes to store mcmc objects in \pkg{coda},
+##' as well as a few new classes.
+##'
+##' @name mcmc4
+##' @docType package
+##' @import plyr
+##' @import reshape2
+##' @import stringr
+##' @import DataFramePlus
+##' @importMethodsFrom stats4 coef vcov
 NULL
 
 ## Classes
