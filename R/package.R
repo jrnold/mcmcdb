32b359939477ea79433e995b13157e62778177fd
Jeffrey Arnold <jeffrey.arnold@gmail.com>
2013-03-13 14:41:21 -0400

    misc

--- R/package.R
+++ R/package.R
@@ -3,12 +3,12 @@
 ##' S4 wrappers of the classes to store mcmc objects in \pkg{coda},
 ##' as well as a few new classes.
 ##'
-##' @name mcmc4
+##' @name mcmcdb
 ##' @docType package
 ##' @import plyr
 ##' @import reshape2
 ##' @import stringr
-##' @import DataFramePlus
+##' @import DataFrameConstr
 ##' @importMethodsFrom stats4 coef vcov
 NULL
 
