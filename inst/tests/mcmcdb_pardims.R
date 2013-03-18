a2662407ebbe95dbd0e97b3dc28fe98732c1fc06
Jeffrey Arnold <jeffrey.arnold@gmail.com>
2013-03-18 15:34:45 -0400

    added mcmcdb_pardims method

---
+++ inst/tests/mcmcdb_pardims.R
@@ -0,0 +1,9 @@
+context("method-mcmcdb_flatpars")
+
+test_McmcdbParameters <-
+  McmcdbParameters(list(alpha = "alpha", beta = c("beta.1", "beta.2")))
+
+test_that("mcmcdb_flatpars,McmcdbParameters-method works as expected", {
+  expect_equal(mcmcdb_pardims(test_McmcdbParameters),
+               list(alpha = 1L, beta = 2L))
+})
