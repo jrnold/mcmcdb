ae89ece331fcaac015344c41d511ff664964c253
Jeffrey Arnold <jeffrey.arnold@gmail.com>
2013-03-18 15:45:29 -0400

    added mcmcdb_paridx method

---
+++ R/method-mcmcdb_par_indices.R
@@ -0,0 +1,40 @@
+#' @include package.R
+#' @include class-McmcdbParameters.R
+#' @exportMethod mcmcdb_paridx
+NULL
+
+#' Get parameter indices
+#'
+#' @param object Object
+#' @return \code{list} of integer matrices. Each element of the
+#' list is a parameter array. Each matrix has a number of rows equal to the
+#' total number of flat parameters, and a number of columns equal
+#' to the number of dimensions of the parameter array. The rownames of
+#' each matrix are the flat parameter names.
+#'
+#' @rdname mcmcdb_paridx-methods
+#' @name mcmcdb_paridx-methods
+#' @aliases mcmcdb_paridx
+#' @aliases mcmcdb_paridx-methods
+NULL
+setGeneric("mcmcdb_paridx",
+           function(object, ...) standardGeneric("mcmcdb_paridx"))
+
+mcmcdb_paridx.McmcdbParameters <- function(object) {
+  # llply returns matrix (why?)
+  ret <- lapply(object,
+                function(x) {
+                  idx <- expand_grid_dim(dim(x))
+                  rownames(idx) <- unlist(x)
+                  idx
+                })
+  names(ret) <- names(object)
+  ret
+}
+
+#' @rdname mcmcdb_paridx-methods
+#' @aliases mcmcdb_paridx,McmcdbParameters-method
+#' @family McmcdbParameters methods
+setMethod("mcmcdb_paridx", "McmcdbParameters",
+          mcmcdb_paridx.McmcdbParameters)
+
---
+++ inst/tests/test-method_mcmcdb_paridx.R
@@ -0,0 +1,14 @@
+context("method-mcmcdb_paridx")
+
+test_McmcdbParameters <-
+  McmcdbParameters(list(alpha = "alpha", beta = c("beta.1", "beta.2")))
+
+test_that("mcmcdb_flatpars,McmcdbParameters-method works as expected", {
+  expected <-
+    structure(list(alpha = structure(1L, .Dim = c(1L, 1L), .Dimnames = list(
+    structure("alpha", .Dim = 1L, class = structure("CharacterArray", package = "mcmcdb")), 
+    "Var1")), beta = structure(1:2, .Dim = c(2L, 1L), .Dimnames = list(
+    structure(c("beta.1", "beta.2"), .Dim = 2L, class = structure("CharacterArray", package = "mcmcdb")), 
+    "Var1"))), .Names = c("alpha", "beta"))
+  expect_equal(mcmcdb_paridx(test_McmcdbParameters), expected)
+})
