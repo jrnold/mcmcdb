12b67e310864ad1216fb6143ae437f0479362218
Jeffrey Arnold <jeffrey.arnold@gmail.com>
2013-03-18 15:28:12 -0400

    added mcmcdb_flatpars method

---
+++ R/method-mcmcdb_flatpars.R
@@ -0,0 +1,34 @@
+#' @include package.R
+#' @include class-McmcdbParameters.R
+#' @exportMethod mcmcdb_flatpars
+NULL
+
+#' Get flat parameter names
+#'
+#' @param object Object
+#' @return Named \code{character} vector. Names are the flat parameter names;
+#' values are the associated parameter arrays.
+#'
+#' @name mcmcdb_flatpars-methods
+#' @rdname mcmcdb_flatpars-methods
+#' @aliases mcmcdb_flatpars
+#' @aliases mcmcdb_flatpars-method
+setGeneric("mcmcdb_flatpars",
+           function(object, ...) standardGeneric("mcmcdb_flatpars"))
+
+mcmcdb_flatpars.McmcdbParameters <- function(object) {
+  flatpars <- unlist(object)
+  pararrays <-
+    unlist(llply(seq_along(object),
+                 function(i) {
+                   rep(names(object)[i], length(object[[i]]))
+                 }))
+  names(pararrays) <- flatpars
+  pararrays
+}
+
+#' @rdname mcmcdb_flatpars-methods
+#' @aliases mcmcdb_flatpars,McmcdbParameters-method
+#' @family McmcdbParameters methods
+setMethod("mcmcdb_flatpars", "McmcdbParameters",
+          mcmcdb_flatpars.McmcdbParameters)
---
+++ inst/tests/test-method-mcmcdb_flatpars.R
@@ -0,0 +1,11 @@
+context("method-mcmcdb_flatpars")
+
+test_McmcdbParameters <-
+  McmcdbParameters(list(alpha = "alpha", beta = c("beta.1", "beta.2")))
+
+test_that("mcmcdb_flatpars,McmcdbParameters-method works as expected", {
+  expect_equal(mcmcdb_flatpars(test_McmcdbParameters),
+               structure(c("alpha", "beta", "beta"),
+                         .Names = c("alpha", "beta.1", "beta.2")))
+
+})
