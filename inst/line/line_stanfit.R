8b071b1e30b0de0cf7087f48f6e8801323dc571f
Jeff Arnold <jeffrey.arnold@gmail.com>
2013-03-19 05:45:31 -0400

    remove stan stuff

--- inst/line/line_stanfit.R
+++
@@ -1,6 +0,0 @@
-library("rstan")
-
-line_data <- read_rdump("line_data.R")
-line_stanfit <- stan("line.stan", data=line_data, seed=1234,
-                     iter=100, chains=2, thin=1)
-save(line_stanfit, file="line_stanfit.Rdata")
