6fe80922aa17680c2cf3d63df0e537759f1447d9
Jeff Arnold <jeffrey.arnold@gmail.com>
2013-03-20 10:03:40 -0400

    added line_samples data

--- inst/line/make_mcmcdbwide.R
+++ inst/line/make_mcmcdbwide.R
@@ -1,6 +1,6 @@
 # create line_mcmcwide data for the package
 library(mcmcdb)
 load("line_mcmc_list.Rdata")
-line <- McmcdbWide(line_mcmc_list)
-line <- list(comment = "For use in the examples of package mcmcdb")
-save(line_mcmcdbwide, file="../../data/line.rda")
+line_samples <- McmcdbWide(line_mcmc_list)
+line_samples@metadata <- list(comment = "For use in the examples of package mcmcdb")
+save(line_samples, file="data/line_samples.rda")
