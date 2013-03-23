a5a803ad85a8df68b319d9a592578a9724119f1a
Jeff Arnold <jeffrey.arnold@gmail.com>
2013-03-23 14:40:34 -0400

    updated checks

--- inst/tests/test-mcmcdb_mcpar.R
+++ inst/tests/test-mcmcdb_mcpar.R
@@ -5,7 +5,7 @@ test_that("mcmcdb_mcpar,McmcdbWide works as expected", {
   mcpars <- mcmcdb_mcpar(test_wide2)
   expect_equal(mcpars,
                data.frame(chain_id = 1:2,
-                          n_iter = rep(2L, 2),
+                          n_iter = rep(3L, 2),
                           iter_start = rep(NA_integer_, 2),
                           iter_end = rep(NA_integer_, 2),
                           iter_thin = rep(NA_integer_, 2)))
