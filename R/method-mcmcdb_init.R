1a9355ff3ce7113db243f0d23bfac4b1a18c1156
Jeffrey Arnold <jeffrey.arnold@gmail.com>
2013-03-27 12:12:17 -0400

    updata mcmcdb_init method

--- R/method-mcmcdb_init.R
+++ R/method-mcmcdb_init.R
@@ -14,18 +14,33 @@ NULL
 #' @param object An object for which the method is defined.
 #' @param flatten \code{logical} Whether to return the parameter values
 #' in their flattened or original array shapes.
-#' @return If \code{flattened=TRUE}, then a named \code{numeric} vector. If
-#' \code{flattened=FALSE}, then a list of numeric \code{array} objects.
+#' @return If \code{flattened=TRUE}, then a \code{matrix}; rows are chains, columns
+#' are flat parameters. If \code{flattened=FALSE}, then a list of
+#' numeric \code{array} objects; the elements of the list are chains, the
+#' arrays are the parameter arrays.
 setGeneric("mcmcdb_init",
            function(object, ...) {
              standardGeneric("mcmcdb_init")
            })
 
+name <- function(x, value) {
+  names(x) <- value
+  x
+}
 
 #' @rdname mcmcdb_init-methods
 #' @aliases mcmcdb_init,McmcdbMem-method
 setMethod("mcmcdb_init", "McmcdbMem",
           function(object, flatten=TRUE) {
-            NULL
+            inits <- daply(object@flatpar_chains, "chain_id",
+                           function(x) {
+                             name(x[["init"]], x[["flatpar"]])
+                           })
+            if (flatten) {
+              inits
+            } else {
+              alply(inits, 1, mcmcdb_unflatten,
+                    parameters = object@parameters)
+            }
           })
 
