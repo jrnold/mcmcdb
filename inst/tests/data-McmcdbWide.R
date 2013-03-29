55cb1da4a2b5c4ddea71f8f4e6ef28895f4f2978
Jeff Arnold <jeffrey.arnold@gmail.com>
2013-03-29 09:31:56 -0400

    add mcmcdb_drop_pararray method

--- inst/tests/data-McmcdbWide.R
+++ inst/tests/data-McmcdbWide.R
@@ -16,3 +16,21 @@ test_wide2 <- local({
   foo <- McmcdbWide(samples, chains = chains, iters = iters,
                     flatpar_chains = flatpar_chains)
 })
+
+test_wide3 <- local({
+  nchains <- 2
+  niter <- 3
+  parnames <- c("beta", "gamma")
+  ncol <- length(parnames)
+  samples <- matrix(seq_len(nchains * niter * ncol), ncol=ncol)
+  colnames(samples) <- parnames
+  chains <- McmcdbChains(chain_id = seq_len(nchains))
+  iters <- McmcdbIters(chain_id = rep(seq_len(nchains), each=niter),
+                       iter = rep(seq_len(niter), nchains))
+  flatpar_chains <-
+    McmcdbFlatparChains(expand.grid(flatpar = parnames,
+                                    chain_id = seq_len(nchains)),
+                        init = 0)
+  foo <- McmcdbWide(samples, chains = chains, iters = iters,
+                    flatpar_chains = flatpar_chains)
+})
